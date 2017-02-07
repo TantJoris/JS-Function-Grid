be614b75c7a5926a02b1f6685809d426f371600b
Travis Joris <tjoris2018@bklawtech.com>
2017-02-07 21:03:12 +0000

    Design Pixels

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-//Write your function calls below this line
+setColor(0 ,0,"purple");
+    setColor(0, 9,"purple");
+        setColor(9, 0,"purple");
+            setColor(9, 9,"purple");
+            
+
+setColor(0,3,"red");
+    setColor(0,2,"red");
+        setColor(2,2,"red");
+            setColor(1,2,"red");
+                setColor(0,4,"red");
+                     setColor(0,5,"red");
+                        setColor(2,2,"red");
+                            setColor(2,3,"red");
+                                setColor(2,4,"red");
+                                    setColor(2,5, "red");
+                                        setColor(3,5 "red");
